c172e3511e46af1d80f7448139252a96cf8b2af6
Lily Steiner <steiner.lily@yahoo.com>
2021-02-11 13:55:00 -0800

    added gene filtering command

--- geneAnalysis/analysis.py
+++ geneAnalysis/analysis.py
@@ -8,3 +8,6 @@ tree = Artifact.load('../tree.qza')
 mdata = Metadata.load('../sample-metadata.txt')
 
 #filtering feature table
+filtResult = feature_table.actions.filter_samples(min_frequency=100000, metadata=mdata, where="[env_material]='feces'",table=fTable)
+filtTable = filtResult.filtered_table
+
