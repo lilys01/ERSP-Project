f87da660c242a4ebb8ec88bce05da2add77d9ff0
Lily Steiner <steiner.lily@yahoo.com>
2021-02-15 11:15:57 -0800

    altered partitioning script

--- geneAnalysis/analysis.py
+++ geneAnalysis/analysis.py
@@ -1,6 +1,9 @@
 import qiime2
-from qiime2.plugins import biodiversity, feature_table, emperor, phylogeny, taxa, empress
+from qiime2.plugins import feature_table, emperor, phylogeny, taxa, empress
 from qiime2 import Artifact, Metadata
+import biom
+import os
+from paritioning import partition
 
 #importing data
 fTable = Artifact.load('genes-ftable.qza')
@@ -10,4 +13,8 @@ mdata = Metadata.load('../sample-metadata.txt')
 #filtering feature table
 filtResult = feature_table.actions.filter_samples(min_frequency=100000, metadata=mdata, where="[env_material]='feces'",table=fTable)
 filtTable = filtResult.filtered_table
+#filtTable.save('filtered-gene-table.qza')
+
+#paritioning 
+
 
---
+++ geneAnalysis/partitioning.py
@@ -0,0 +1,61 @@
+"""Extract marker genes from Woltka-generated per-gene feature table.
+
+Usage: parition(input.biom, gene_to_marker.map, output_dir)
+
+Adapation of /projects/wol/woltka/extract_markers.py 
+
+Output:
+    One BIOM table per marker gene.
+"""
+
+from sys import argv
+from os import makedirs
+from os.path import join
+from biom import Table, load_table
+from biom.util import biom_open
+
+
+def partition(biomTable, map, outputDir):
+
+    # load input BIOM table
+    table = load_table(biomTable)
+    samples = table.ids(axis='sample')
+    genby = table.generated_by
+
+    # read gene-to-marker mapping
+    marker_dict = {}
+    with open(map, 'r') as f:
+        g = None
+        for line in f:
+            line = line.rstrip()
+            if line.startswith('>'):
+                g = line[1:]
+                marker_dict[g] = {}
+            else:
+                idx, marker = line.split('\t')
+                marker_dict[g][idx] = marker
+
+    # extract marker genes from table
+    tables = {}
+    for gene, row in zip(table.ids(axis='observation'),
+                         table.iter_data(dense=True, axis='observation')):
+        g, _, idx = gene.partition('_')
+        mdic = marker_dict[g]
+        if idx in mdic:
+            marker = mdic[idx]
+            if marker not in tables:
+                tables[marker] = [g], [row]
+            else:
+                tables[marker][0].append(g)
+                tables[marker][1].append(row)
+
+    # write per-marker tables
+    outdir = outputDir
+    makedirs(outdir, exist_ok=True)
+    for marker, (gs, rows) in tables.items():
+        with biom_open(join(outdir, f'{marker}.biom'), 'w') as f:
+            Table(rows, gs, samples).to_hdf5(f, genby)
+
+
+if __name__ == '__main__':
+    main()
