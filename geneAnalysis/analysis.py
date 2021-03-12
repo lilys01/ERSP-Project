04740856bd1977501adc16f44c674bb7703d81de
Lily Steiner <steiner.lily@yahoo.com>
2021-03-11 19:31:47 -0800

    per gene unifrac

--- geneAnalysis/analysis.py
+++ geneAnalysis/analysis.py
@@ -1,10 +1,12 @@
 import qiime2
 from qiime2.plugins import diversity, feature_table, emperor, phylogeny, taxa
+from qiime2.plugins.feature_table.methods import rarefy
 from qiime2 import Artifact, Metadata
 import biom
 import os
 import glob
 import pandas as pd
+import numpy as np 
 from partitioning import partition
 
 #importing data
@@ -13,70 +15,98 @@ tree = Artifact.load('../tree.qza')
 mdata = Metadata.load('../sample-metadata.txt')
 
 '''
-#filtering feature table
-filtResult = feature_table.actions.filter_samples(min_frequency=100000, metadata=mdata, where="[env_material]='feces'",table=fTable)
-filtTable = filtResult.filtered_table
-#filtTable.save('filtered-gene-table.qza')
-
-#paritioning 
-btable = filtTable.view(biom.Table)
-partition(btable,'/projects/wol/release/markers/phylophlan.intree.map', 'gene-tables') 
+Filters and partitions tables
 '''
+def gentables():
+    
+    #filtering feature table
+    filtResult = feature_table.actions.filter_samples(min_frequency=100000, metadata=mdata, where="[env_material]='feces'",table=fTable)
+    filtTable = filtResult.filtered_table
+
+    filtTable.save('filtered-gene-table.qza')
+
+    #paritioning 
+    btable = filtTable.view(biom.Table)
+    partition(btable,'/projects/wol/release/markers/phylophlan.intree.map', 'gene-tables') 
+
+
+
 #set dictionaries with gene-tree and gene-table pairs for easy look up
+def setDictionaries():
+   tablelookup = {}
+   for f in glob.glob('gene-tables/*.qza'):
+     basename = os.path.basename(f)
+     no_extension = os.path.splitext(basename)[0]
+     tablelookup[no_extension] = f
 
-tablelookup = {}
-for f in glob.glob('gene-tables/*.qza'):
- basename = os.path.basename(f)
- no_extension = os.path.splitext(basename)[0]
- tablelookup[no_extension] = f
+   treelookup = {}
+   for t in glob.glob('trees/*.nwk'):
+     base = os.path.basename(t)
+     no_ext = os.path.splitext(base)[0]
+     treelookup[no_ext] = t 
 
-treelookup = {}
-for t in glob.glob('trees/*.nwk'):
- base = os.path.basename(t)
- no_ext = os.path.splitext(base)[0]
- treelookup[no_ext] = t 
-
-'''for key in tablelookup.keys():'''
-key='p0097'
-newtable = Artifact.load(tablelookup.get(key))
-newtree = qiime2.Artifact.import_data('Phylogeny[Rooted]',treelookup.get(key))
-
-distMatrix = diversity.pipelines.beta_phylogenetic(table=newtable, phylogeny=newtree, metric='unweighted_unifrac')
-dm = distMatrix.distance_matrix
-
-PCoA = diversity.actions.pcoa(distance_matrix=dm)
-pcoaResults = PCoA.pcoa 
 
 '''
-viz = emperor.actions.plot(pcoaResults, mdata)
-viz.visualization.save('p0097_emperor.qzv')
 
-summary = feature_table.actions.summarize(table=newtable, sample_metadata=mdata)
-summary.visualization.save('p0097_summary.qzv')
-'''
+compute unweughted unifrac on each gene with it's table and tree
+compute pcoa and make an emperor plot for each
+add a new column for gene read counts to the metadata
 
-'''Add a new category column to our metadata'''
+ '''
+
+def perGeneAnalysis(key):
+   #print(key) 
+   tablelookup = {}
+   for f in glob.glob('gene-tables/*.qza'):
+     basename = os.path.basename(f)
+     no_extension = os.path.splitext(basename)[0]
+     tablelookup[no_extension] = f
+
+   treelookup = {}
+   for t in glob.glob('trees/*.nwk'):
+     base = os.path.basename(t)
+     no_ext = os.path.splitext(base)[0]
+     treelookup[no_ext] = t
+
+   newtable = Artifact.load(tablelookup.get(key))
+   newtree = qiime2.Artifact.import_data('Phylogeny[Rooted]',treelookup.get(key))
+
+   bTable = qiime2.Artifact.load(tablelookup.get(key)).view(biom.Table)
+
+   #rarefraction on gene table
+   #quartile,bins = pd.qcut(bTable.sum('sample'), 4, retbins=True)
+   quartile = pd.qcut(bTable.sum('sample'),4,labels=False)
+   rarefyFt, = feature_table.methods.rarefy(newtable, int(quartile[3]))
+
+   #unifrac
+   distMatrix = diversity.pipelines.beta_phylogenetic(table=rarefyFt, phylogeny=newtree, metric='unweighted_unifrac')
+   dm = distMatrix.distance_matrix
+   dm.save('gene-dms/' + key + '-dist-matrix.qza')
+
+   PCoA = diversity.actions.pcoa(distance_matrix=dm)
+   pcoaResults = PCoA.pcoa 
 
 
-'''df = pd.DataFrame('../sample-metadata.txt')'''
-df = mdata.to_dataframe()
+   #add column of gene counts to metadata
 
-'''bTable = biom.load_table('gene-tables/p0097.biom')'''
-bTable = qiime2.Artifact.load('gene-tables/p0097.qza').view(biom.Table)
+   df = mdata.to_dataframe()
 
-newCategory = pd.Series(bTable.sum('sample'), index=bTable.ids())
-df['p0097-read-counts'] = newCategory
+   newCategory = pd.Series(bTable.sum('sample'), index=bTable.ids())
 
-new_md = qiime2.Metadata(df)
+   columnName = key + '-read-counts'
 
-viz = emperor.actions.plot(pcoaResults, new_md)
-viz.visualization.save('p0097_emperor.qzv')
+   df[columnName] = newCategory
 
-summary = feature_table.actions.summarize(table=newtable, sample_metadata=new_md)
-summary.visualization.save('p0097_summary.qzv')
+   new_md = qiime2.Metadata(df)
+
+   viz = emperor.actions.plot(pcoaResults, new_md)
+   viz.visualization.save('gene-emp-plots/' + key + '-emperor.qzv')
+  
+
+def summarize():
+   summary = feature_table.actions.summarize(table=newtable, sample_metadata=mdata)
+   summary.visualization.save('p0097_summary.qzv')
 
 
 
 
-
- 
---
+++ geneAnalysis/processor.py
@@ -0,0 +1,19 @@
+import click
+from analysis import perGeneAnalysis, setDictionaries
+
+
+@click.command()
+@click.option('--parametersfile', type=click.Path(exists=True), required=True)
+@click.option('--taskid', type=int, required=True)
+
+def process(parametersfile, taskid):
+    details = open(parametersfile).readlines()
+    geneName = details[taskid - 1]
+    geneName = ''.join(geneName.split())
+    perGeneAnalysis(geneName)
+
+
+if __name__ == '__main__':
+    process()
+
+
